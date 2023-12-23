9b2005fe661dff18323a1856022ad4757ffeefb0
ajayayrekaraj <ajayayrekaraj@gmail.com>
2023-12-24 01:01:31 +0530

    C Code

--- Assignment-No.3/Program16.c
+++ Assignment-No.3/Program16.c
@@ -3,4 +3,17 @@
 
 #include<stdio.h>
 void main(){
-int
+	char ch;
+	printf("Enter a Character:");
+	scanf("%c",&ch);
+
+	if(ch>='a' && ch<='z')
+		printf("Lowercase\n");
+	else if(ch>='A' && ch<='Z')
+		printf("UpperCase\n");
+	else if(ch>='0' && ch<='9')
+		printf("Digit\n");
+	else
+		printf("Special Character\n");
+}
+
